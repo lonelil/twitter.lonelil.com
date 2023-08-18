74c2d5ae386affb43ce925476f9d9c737e5e893d
lonelil <51315646+lonelil@users.noreply.github.com>
2023-08-19 04:39:55 +0800

    cool shit

--- app/[username]/status/[id]/page.tsx
+++ app/[username]/status/[id]/page.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import * as cheerio from "cheerio";
 import Script from "next/script";
 
+const TWEET_ID = /^[0-9]+$/;
+
+function getToken(id: string) {
+  return ((Number(id) / 1e15) * Math.PI)
+    .toString(6 ** 2)
+    .replace(/(0+|\.)/g, "");
+}
+
 export async function generateMetadata({
   params,
 }: {
@@ -10,7 +18,42 @@ export async function generateMetadata({
     id: string;
   };
 }): Promise<Metadata> {
-  const dataRequest = await fetch(
+  if (params.id.length > 40 || !TWEET_ID.test(params.id)) {
+    throw new Error(`Invalid tweet id: ${params.id}`);
+  }
+
+  const url = new URL("https://cdn.syndication.twimg.com/tweet-result");
+
+  url.searchParams.set("id", params.id);
+  url.searchParams.set("lang", "en");
+  url.searchParams.set(
+    "features",
+    [
+      "tfw_timeline_list:",
+      "tfw_follower_count_sunset:true",
+      "tfw_tweet_edit_backend:on",
+      "tfw_refsrc_session:on",
+      "tfw_fosnr_soft_interventions_enabled:on",
+      "tfw_show_birdwatch_pivots_enabled:on",
+      "tfw_show_business_verified_badge:on",
+      "tfw_duplicate_scribes_to_settings:on",
+      "tfw_use_profile_image_shape_enabled:on",
+      "tfw_show_blue_verified_badge:on",
+      "tfw_legacy_timeline_sunset:true",
+      "tfw_show_gov_verified_badge:on",
+      "tfw_show_business_affiliate_badge:on",
+      "tfw_tweet_edit_frontend:on",
+    ].join(";")
+  );
+  url.searchParams.set("token", getToken(params.id));
+
+  const dataRequest = await fetch(url.toString(), {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
+    },
+  });
+  const pageRequest = await fetch(
     `https://twitter.com/${params.username}/status/${params.id}`,
     {
       headers: {
@@ -20,73 +63,83 @@ export async function generateMetadata({
     }
   );
 
-  const data = await dataRequest.text();
+  const page = await pageRequest.text();
+  const data = await dataRequest.json();
 
-  const $ = cheerio.load(data);
+  const $ = cheerio.load(page);
 
-  const text = (id: string) => {
-    return $(`[data-testid=${id}]`).first().text();
-  };
+  const displayName = data.user.name;
+  const handle = "@" + data.user.screen_name;
 
+  const siteName = `twitter.lonelil.com · ${new Date(
+    data.created_at
+  ).toLocaleString()}`;
+  const communityNotes = data.birdwatch_pivot;
+
+  const description = `${data.text}${
+    communityNotes
+      ? `\n\n👪 ${communityNotes.title}\n\n${communityNotes.subtitle}`
+      : ""
+  }`;
   const dataAttr = (num: number) => {
     return $(`[data-testid=app-text-transition-container] > :eq(${num})`)
       .first()
       .text();
   };
 
-  const displayName = $(`[data-testid=User-Name] > :eq(0)`).first().text();
-  const handle = $(`[data-testid=User-Name] > :eq(1)`).first().text();
-
-  const siteName = `twitter.lonelil.com · ${$("time")
-    .last()
-    .text()
-    .split(" · ")
-    .reverse()
-    .join(" - ")}`;
-
-  const tweetPhotos = $('[data-testid="tweetPhoto"]');
-  let imageLinks: string[] = [];
-
-  tweetPhotos.each(function (i, el) {
-    let imageLink = $(el).children().last().attr("src")?.split("?")[0] + ".jpg";
-    imageLinks.push(imageLink);
-  });
-
   return {
     title: "",
     description: "",
+    themeColor:
+      /*data.photos
+      ? rgbToHex(
+          data.photos[0].backgroundColor.red,
+          data.photos[0].backgroundColor.green,
+          data.photos[0].backgroundColor.blue
+        )
+      : */ "#2B2D31",
     openGraph: {
       siteName,
-      description: `${text("tweetText")}${
-        $(`[data-testid=birdwatch-pivot]`).length > 0
-          ? `\n\n👪 𝗖𝗼𝗺𝗺𝘂𝗻𝗶𝘁𝘆 𝗡𝗼𝘁𝗲𝘀\n\n${$(
-              `[data-testid=birdwatch-pivot] > :eq(2)`
-            ).text()}`
-          : ""
-      }`,
+      description,
+      videos: data.video && {
+        url: data.video.variants[0].src,
+        type: data.video.variants[0].type,
+      },
     },
     twitter: {
-      card: "summary_large_image",
+      card: data.video ? "player" : "summary_large_image",
       title: `${displayName} ${
-        $(`[data-testid=icon-verified]`) ? "✅ " : ""
+        data.user.verified
+          ? "✅ "
+          : data.user.is_blue_verified
+          ? "🟦 "
+          : data.user.verified_type === "Business"
+          ? "🟨 "
+          : ""
       }(${handle})`,
+      site: handle,
       creator: handle,
-      images:
-        tweetPhotos.length === 1
-          ? imageLinks[0]
-          : tweetPhotos.length > 1
-          ? `https://vxtwitter.com/rendercombined.jpg?imgs=${imageLinks.join(
-              ","
-            )}`
-          : "",
+      images: data.photos
+        ? data.photos.length === 1
+          ? data.photos[0].url
+          : data.photos.length > 1
+          ? `https://vxtwitter.com/rendercombined.jpg?imgs=${data.photos
+              .map((photo: any) => {
+                return photo.url;
+              })
+              .join(",")}`
+          : ""
+        : "",
     },
     alternates: {
       types: {
         "application/json+oembed": `https://webembed-sb.onrender.com/oembed?author_name=${encodeURIComponent(
           `👀 ${dataAttr(0)} ♻️ ${dataAttr(1)} 💬 ${dataAttr(2)} 👍 ${dataAttr(
             3
-          )} 🔖 ${dataAttr(4)}`
-        )}&author_url=https://twitter.com/${handle}&provider_name=${encodeURIComponent(siteName)}&provider_url=https://lonelil.com`,
+          )} 🔖 ${dataAttr(4)}${data.video ? `\n\n${description}` : ""}`
+        )}&author_url=https://twitter.com/${handle}&provider_name=${encodeURIComponent(
+          siteName
+        )}&provider_url=https://lonelil.com`,
       },
     },
   };
@@ -102,12 +155,14 @@ export default function Page({
 }) {
   return (
     <>
-      <Script id="redirect" strategy="beforeInteractive">
-        {`
+      {process.env.NODE_ENV === "production" && (
+        <Script id="redirect" strategy="beforeInteractive">
+          {`
         const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
         window.location.replace(isMobile ? "twitter://status?id=${params.id}" : "https://twitter.com/${params.username}/status/${params.id}")
         `}
-      </Script>
+        </Script>
+      )}
     </>
   );
 }
